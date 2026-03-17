82e31c9e05a7aba6153116121f0c40f046a0cb42
Catherine707 <catherinecoti18@gmail.com>
2026-03-16 22:30:05 -0600

    chore(assignment-05): agregar script de build para doppler en frontend

---
+++ frontend/render-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o errexit
+
+apt-get update
+apt-get install -y curl gnupg
+(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh
+
+doppler secrets download --no-file --format env > .env.production
+npm install
+node node_modules/vite/bin/vite.js build
